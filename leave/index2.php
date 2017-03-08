9bb171035c1dfd186ca6babdc5c3a8989f5c2718
raghvendra.yadav <raghvendra.yadav@9a3d8878-9123-1b48-bcc6-7bac9082d0fe>
2017-03-08 05:44:37 +0000

    git-svn-id: https://versioncontrol.techtreeit.com/svn/Team_PHP/JIRA_Reports@3062 9a3d8878-9123-1b48-bcc6-7bac9082d0fe

---
+++ leave/index2.php
@@ -0,0 +1,11 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+?>
+<meta name="ajs-remote-user" content="raghvendra.yadav">
+<iframe height="400px"  width ="100%"  x-frame-options="NONE" src="http://localhost/JIRA_Reports/leave-form/form/index.html"></iframe>
